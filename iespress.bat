17f7f9d67607cd98916c23fd650577620221c7f5
Mark Gollnick <mark.r.gollnick@gmail.com>
2014-11-08 18:29:40 -0600

    Fix iespress.bat
    Oops...

--- iespress.bat
+++ iespress.bat
@@ -5,10 +5,10 @@ for /F "delims=" %%A in ("%ARG0%") do set THIS=%%~nxA
 for /F "delims=" %%A in ("%ARG0%") do set WDIR=%%~dpA
 set WDIR=%WDIR:~0,-1%
 
-for /F "delims=" %%A in ('cscript "%WDIR%\pexec.vbs" 3') do set WDIR=%%~dpA
+for /F "delims=" %%A in ('cscript "%WDIR%\iespress.vbs" 3') do set WDIR=%%~dpA
 set WDIR=%WDIR:~0,-1%
 
-xcopy /y * "%WDIR%\" /EXCLUDE:pcopy.bat+pexec.vbs
+xcopy /y * "%WDIR%\" /EXCLUDE:iespress.bat+iespress.vbs
 
 cd "%WDIR%"
 for /F "delims=" %%A in ("%1") do start "%%~A"
